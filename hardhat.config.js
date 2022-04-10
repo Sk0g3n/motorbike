6ad33b05b115bbfecbaba4d7444a8715d0d3a1ec
Sk0g3n <102603178+Sk0g3n@users.noreply.github.com>
2022-04-10 19:24:26 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,6 @@
  
    networks: {
      rinkeby: {
-       url: 'https://rinkeby.infura.io/v3/8aa2106d23d8414eaa44ba4cc0a07267',
-       accounts: ['64f1f2acaf2de8aaf1b707ae82b00f1618679b19829b2112d03f127749a31c53'],
      },
    }
  };
